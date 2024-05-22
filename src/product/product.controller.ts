910d768b945c75861462423c9aef69d754b797b8
Microdev12 <singhaditya0407@gmail.com>
2024-05-22 18:31:11 +0530

    product controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ProdcutService } from 'src/prodcut/prodcut.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    constructor(private productService : ProdcutService) {}
+
+    @Post()
+    registerUsers() : any {
+        
+    }
+}
